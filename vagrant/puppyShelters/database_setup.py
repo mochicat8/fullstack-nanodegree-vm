897a520b46135d1f34bf31c48c9acc519dd66d81
mochicat8 <mochicat8@gmail.com>
2016-05-01 17:37:15 -0700

    updates code and adds script to populate database with dummy data

--- vagrant/puppyShelters/database_setup.py
+++ vagrant/puppyShelters/database_setup.py
@@ -1,7 +1,7 @@
 # Provides a number of functions and variables
 import sys
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Numeric
 
 from sqlalchemy.ext.declarative import declarative_base
 #used when having the mapper
@@ -22,23 +22,26 @@ class Shelter(Base):
 	#Mapper Functions
 	#Establishes a column in the database
 	#Attributes are columns
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	address = Column(String(250), nullable=False)
-	city = Column(String(250), nullable=False)
-	zipCode = Column(Varchar(5), nullable=False)
-	website = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key = True)
+    name =Column(String(80), nullable = False)
+    address = Column(String(250))
+    city = Column(String(80))
+    state = Column(String(20))
+    zipCode = Column(String(10))
+    website = Column(String)
 
 
 class Puppy(Base):
 	__tablename__= 'puppy'
 	#Mapper Functions
-	name = Column(String(80), nullable=False)
-	date_of_birth = Column(Date, nullable=False)
-	gender = Column(Boolean)
-	weight = Column(Varchar(3), nullable=False)
-	shelter_id = Column(Integer, ForeignKey('shelter.id'))
-	shelter = relationship(Shelter)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(6), nullable = False)
+    dateOfBirth = Column(Date)
+    picture = Column(String)
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
+    shelter = relationship(Shelter)
+    weight = Column(Numeric(10))
 
 #### End
 engine = create_engine('sqlite:///puppies.db')
