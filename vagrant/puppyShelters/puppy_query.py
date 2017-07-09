78c0e480753a414454a905dd907c721c5f225bd0
mochicat8 <mochicat8@gmail.com>
2017-07-09 00:05:36 -0700

    adds a join query

--- vagrant/puppyShelters/puppy_query.py
+++ vagrant/puppyShelters/puppy_query.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy import func
 from sqlalchemy.orm import sessionmaker
 
 from puppies import Base, Shelter, Puppy
@@ -28,3 +29,8 @@ for instance in session.query(Puppy).filter(Puppy.dateOfBirth > datetime.date.to
 print "Printing Puppy Weight"
 for instance in session.query(Puppy).order_by(Puppy.weight):
     print (instance.name, str(round(instance.weight,2)))
+
+
+print "Printing Puppy Shelter"
+for instance1, instance2, instance3 in session.query(Puppy, Shelter, func.count(Puppy.shelter_id)).filter(Puppy.shelter_id == Shelter.id).group_by(Shelter.id).all():
+    print (instance2.name, instance3)
