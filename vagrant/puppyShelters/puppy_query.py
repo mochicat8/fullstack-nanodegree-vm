7a4dde4ca0a049b1dad4a0ef37b0c8cb8ff24585
mochicat8 <mochicat8@gmail.com>
2017-07-08 23:39:48 -0700

    modifies queries

--- vagrant/puppyShelters/puppy_query.py
+++ vagrant/puppyShelters/puppy_query.py
@@ -16,10 +16,15 @@ DBSession = sessionmaker(bind=engine)
 
 session = DBSession()
 
-
+print "Printing Name (Alphabetically)"
 for instance in session.query(Puppy).order_by(Puppy.name):
+
     print instance.name + " " +  instance.gender
 
-
+print "Printing Name (Birth < 6 months)"
 for instance in session.query(Puppy).filter(Puppy.dateOfBirth > datetime.date.today() - datetime.timedelta(days = 180)).order_by(-Puppy.dateOfBirth):
-    print (instance.name, instance.dateOfBirth)
+    print (instance.name, instance.dateOfBirth.isoformat())
+
+print "Printing Puppy Weight"
+for instance in session.query(Puppy).order_by(Puppy.weight):
+    print (instance.name, str(round(instance.weight,2)))
