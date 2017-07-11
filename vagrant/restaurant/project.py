11dd3ae60e773b16b1d9c52018ff39a1a432229b
mochicat8 <mochicat8@gmail.com>
2017-07-11 16:02:53 -0700

    templating edits

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,34 +1,46 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+app = Flask(__name__)
+# Create an instance of the Flask class with name of running application
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+## Following function called HELLO world will be executed if any calls from the server have the route of / or /hello
 @app.route('/')
-@app.route('/hello')
-
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
+    return render_template('menu.html', restaurant = restaurant, items = items)
+# Task 1: Create route for newMenuItem function here
 
-    return output
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
 
+# Task 2: Create route for editMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+# If not used as an imported module run following code
 if __name__ == '__main__':
+    # Server reload on code changes
     app.debug = True
+    #
     app.run(host = '0.0.0.0', port = 5000)
