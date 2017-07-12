2436a7896264239c103842dbc012c5f33784bef4
mochicat8 <mochicat8@gmail.com>
2017-07-12 15:40:47 -0700

    pep8 fixes

--- vagrant/restaurant/project.py
+++ vagrant/restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect, flash
 
 
 from sqlalchemy import create_engine
@@ -14,33 +14,78 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-## Following function called HELLO world will be executed if any calls from the server have the route of / or /hello
+# Following function called HELLO world will be executed if any calls from the
+# server have the route of / or /hello
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    return render_template('menu.html', restaurant = restaurant, items = items)
-# Task 1: Create route for newMenuItem function here
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
-@app.route('/restaurants/<int:restaurant_id>/new/')
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'],
+                           description=request.form['description'],
+                           price=request.form['price'],
+                           course=request.form['course'],
+                           restaurant_id=restaurant_id)
+        flash("new menu item created!")
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu'), restaurant_id=restaurant_id)
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
+
 
 # Task 2: Create route for editMenuItem function here
-
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/',
+           methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    oldName = editedItem.name
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        flash("name changed from %s to %s" % (oldName, request.form['name']))
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template('editmenuitem.html',
+                               restaurant_id=restaurant_id,
+                               menu_id=menu_id,
+                               item=editedItem)
+
 
 # Task 3: Create a route for deleteMenuItem function here
-
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/',
+           methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+    deleteItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    print request.method
+    if request.method == 'POST':
+        session.delete(deleteItem)
+        session.commit()
+        flash('Item deleted: %s' % deleteItem.name)
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template('deletemenuitem.html',
+                               restaurant_id=restaurant_id,
+                               menu_id=menu_id,
+                               item=deleteItem)
+
 # If not used as an imported module run following code
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     # Server reload on code changes
     app.debug = True
     #
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
